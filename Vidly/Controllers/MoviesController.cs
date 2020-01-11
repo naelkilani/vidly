a97b2392c128db34e890408c0d41088c69653b05
Nael Kilani <NaelKilanii@gmail.com>
2020-01-12 00:19:20 +0200

    Working with Action Parameters.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using Microsoft.Ajax.Utilities;
+using System.Web.Mvc;
 using Vidly.Models;
 
 namespace Vidly.Controllers
@@ -10,6 +11,17 @@ namespace Vidly.Controllers
         {
             var movie = new Movie { Id = 1, Name = "Shark" };
             return RedirectToAction("Index", "Home", new { Page = 1, SortBy = "name" });
+        }
+
+        public ActionResult Index(int? page, string sortBy)
+        {
+            if (!page.HasValue)
+                page = 1;
+
+            if (sortBy.IsNullOrWhiteSpace())
+                sortBy = "name";
+
+            return Content($"page = {page} & sortBy = {sortBy}");
         }
     }
 }
