0654fec0ee48ceebe586bb5f846d071219ab57b7
Nael Kilani <NaelKilanii@gmail.com>
2020-01-12 00:00:59 +0200

    Test MVC in action.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie { Id = 1, Name = "Shark" };
+            return View(movie);
+        }
+    }
+}
