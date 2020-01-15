b41e53b0dd7672e8d7e3319ecaa6444c11f88ac2
Nael Kilani <NaelKilanii@gmail.com>
2020-01-16 00:21:44 +0200

    Add Name To Membership

--- Vidly/Models/Membership.cs
+++ Vidly/Models/Membership.cs
@@ -5,6 +5,7 @@ namespace Vidly.Models
     public class Membership
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public byte DurationInMonths { get; set; }
         public decimal SignUpFee { get; set; }
         public decimal DiscountRate { get; set; }
