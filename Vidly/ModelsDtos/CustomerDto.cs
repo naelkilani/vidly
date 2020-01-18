8e6933e3c951078a87e1d4da2d3fb13091de24ed
Nael Kilani <NaelKilanii@gmail.com>
2020-01-19 00:06:56 +0200

    Fix the labels of customer form after using customerdto.

--- Vidly/ModelsDtos/CustomerDto.cs
+++ Vidly/ModelsDtos/CustomerDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Vidly.Models;
 
 namespace Vidly.ModelsDtos
@@ -6,10 +7,19 @@ namespace Vidly.ModelsDtos
     public class CustomerDto
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
+
         public Membership Membership { get; set; }
+
+        [Display(Name = "Membership Type")]
         public int MembershipId { get; set; }
     }
 }
